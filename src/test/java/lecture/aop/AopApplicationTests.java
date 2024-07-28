9adae0fc31b7d76600f95fde8d1cc0a1d5d8b512
hongsh429 <shh0429@kakao.com>
2024-07-28 09:45:43 +0900

    first commit

---
+++ src/test/java/lecture/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package lecture.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
