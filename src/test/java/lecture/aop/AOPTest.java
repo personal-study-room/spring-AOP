419f47e9323a6318be43e6ed64d0f71f65bf8e68
hongsh429 <shh0429@kakao.com>
2024-07-28 10:10:58 +0900

    AspectV1 적용 후 테스트 코드 확인

--- src/test/java/lecture/aop/AOPTest.java
+++ src/test/java/lecture/aop/AOPTest.java
@@ -2,18 +2,21 @@ package lecture.aop;
 
 import lecture.aop.order.OrderRepository;
 import lecture.aop.order.OrderService;
+import lecture.aop.order.aop.AspectV1;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestConstructor;
 
 @Slf4j
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 @RequiredArgsConstructor
 @SpringBootTest
+@Import(AspectV1.class) // 간단하게 스프링 빈에 추가하는 방법. 주로 설정 파일을 추가할 때 사용
 public class AOPTest {
 
   private final OrderService orderService;
@@ -24,11 +27,6 @@ public class AOPTest {
   void aopInfo() {
     log.info("isAOPProxy, orderService={}", AopUtils.isAopProxy(orderService));
     log.info("isAOPProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
-
-    /**
-     * 2024-07-28T09:59:42.346+09:00  INFO 9774 --- [aop] [           main] lecture.aop.AOPTest                      : isAOPProxy, orderService=false
-     * 2024-07-28T09:59:42.346+09:00  INFO 9774 --- [aop] [           main] lecture.aop.AOPTest                      : isAOPProxy, orderRepository=false
-     */
   }
 
   @Test
